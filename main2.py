ce3289eb3a38f2e17f26962cb95ba8499b7b7a39
Larry Guan <haihui.guan@samsung.com>
2018-07-05 15:02:13 -0700

    first finished version

--- main2.py
+++ main2.py
@@ -6,7 +6,7 @@ import os
 import numpy as np
 from skimage.color import rgb2gray
 import random
-
+import pandas as pd
 # Import `tensorflow`
 import tensorflow as tf
 
@@ -29,36 +29,6 @@ def load_data(data_dir):
             labels.append(int(d))
     return images, labels
 
-ROOT_PATH = "/home/hguan/project/machine-learning/garage/"
-train_data_dir = os.path.join(ROOT_PATH, "training")
-test_data_dir = os.path.join(ROOT_PATH, "testing")
-
-images, labels = load_data(train_data_dir)
-
-images_array = np.array(images)
-labels_array = np.array(labels)
-print(len(set(labels_array)))
-
-# Resize images
-images32 = [transform.resize(image, (28, 28)) for image in images]
-images32 = np.array(images32)
-images32 = rgb2gray(np.array(images32))
-print(images32.shape)
-
-# Parameters
-learning_rate = 0.01
-training_epochs = 25
-batch_size = 2
-display_step = 1
-
-# tf Graph Input
-x = tf.placeholder(tf.float32, [None, 784]) # mnist data image of shape 28*28=784
-y = tf.placeholder(tf.float32, [None, 2]) # 0-9 digits recognition => 10 classes
-
-# Set model weights
-W = tf.Variable(tf.zeros([784, 10]))
-b = tf.Variable(tf.zeros([10]))
-
 flags = tf.app.flags
 FLAGS = flags.FLAGS
 
@@ -66,16 +36,50 @@ flags.DEFINE_integer('num_classes', 2, 'Number of classes.')
 flags.DEFINE_integer('num_variables', 784, 'Number of variables.')
 
 # Hyper Parameters
-flags.DEFINE_integer('hidden1', 2048, 'Number of units in hidden layer 1.')
-flags.DEFINE_integer('hidden2', 1024, 'Number of units in hidden layer 2.')
-flags.DEFINE_integer('num_epochs', 200, 'Number of learning epochs.')
-flags.DEFINE_integer('batch_size', 90, 'Batch size.')
+flags.DEFINE_integer('hidden1', 256, 'Number of units in hidden layer 1.')
+flags.DEFINE_integer('hidden2', 128, 'Number of units in hidden layer 2.')
+flags.DEFINE_integer('num_epochs', 50, 'Number of learning epochs.')
+flags.DEFINE_integer('batch_size', 10, 'Batch size.')
 flags.DEFINE_float('keep_prob', 0.5, 'Keep probability for drop out.')
-flags.DEFINE_float('learning_rate', 0.001, 'Initial learning rate.')
+flags.DEFINE_float('learning_rate', 0.01, 'Initial learning rate.')
+
+def inference(data, data_size, keep_prob):
+    # # Hidden layer 1
+    # with tf.name_scope('hidden1'):
+    #     weights = tf.Variable(tf.truncated_normal([data_size, FLAGS.hidden1],
+    #                                               stddev=1.0 / math.sqrt(float(data_size))), name='weights1')
+    #     biases = tf.Variable(tf.zeros([FLAGS.hidden1]), name='biases1')
+    #     hidden1 = tf.nn.relu(tf.matmul(data, weights) + biases)
+    #
+    #     # Dropout before layer 2
+    #     hidden1_drop = tf.nn.dropout(hidden1, keep_prob, name='layer1_dropout')
+    #
+    # # Hidden layer 2
+    # with tf.name_scope('hidden2'):
+    #     weights = tf.Variable(tf.truncated_normal([FLAGS.hidden1, FLAGS.hidden2],
+    #                                               stddev=1.0 / math.sqrt(float(FLAGS.hidden1))), name='weights2')
+    #     biases = tf.Variable(tf.zeros([FLAGS.hidden2]), name='biases2')
+    #     hidden2 = tf.nn.sigmoid(tf.matmul(hidden1_drop, weights) + biases)
+    #
+    #     # Dropout before linear reading out
+    #     hidden2_drop = tf.nn.dropout(hidden2, keep_prob, name='layer2_dropout')
+    #
+    # # Read out
+    # with tf.name_scope('softmax_linear'):
+    #     weights = tf.Variable(tf.truncated_normal([FLAGS.hidden2, FLAGS.num_classes],
+    #                                               stddev=1.0 / math.sqrt(float(FLAGS.hidden2))), name='weights')
+    #     biases = tf.Variable(tf.zeros([FLAGS.num_classes]), name='biases')
+    #     logits = tf.matmul(hidden2_drop, weights) + biases
+
+        weights = tf.Variable(tf.zeros([data_size, FLAGS.num_classes]), name='weights')
+        biases = tf.Variable(tf.zeros([FLAGS.num_classes]), name='biases')
+        logits = tf.matmul(data, weights) + biases
+
+        return logits
 
 def loss(logits, labels):
     labels = tf.to_int64(labels)
-    cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits, labels, name='xentropy')
+    cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=labels, name='xentropy')
     loss = tf.reduce_mean(cross_entropy, name='xentropy_mean')
     return loss
 
@@ -86,16 +90,43 @@ def training(loss):
     train_op = optimizer.minimize(loss, global_step=global_step)
     return train_op
 
+def evaluation(logits, labels):
+    correct = tf.nn.in_top_k(logits, labels, 1)
+    return tf.reduce_sum(tf.cast(correct, tf.int32))
+
+def shuffle_data(data, labels):
+    # transform list into DataFrame
+    new_df = pd.DataFrame(data)
+    new_df['__labels__'] = labels
+    new_df = new_df.reindex(np.random.permutation(new_df.index))
+
+    new_labels = list(new_df['__labels__'])
+    del new_df['__labels__']
+
+    # transform DataFrame into list
+    new_row = []
+    for index, row in new_df.iterrows():
+        _list_row = []
+        for col in new_df:
+            _list_row.append(row[col])
+        new_row.append(_list_row)
+
+    return new_row, new_labels
+
 def run_training(data, labels):
     # Tell TensorFlow that the model will be built into the default Graph.
     with tf.Graph().as_default():
         data_size = FLAGS.num_variables
         num_classes = FLAGS.num_classes
-        w = tf.Variable(tf.zeros([784, 2])
-        b = tf.Variable(tf.zeros([2]))
+
+        data_placeholder = tf.placeholder("float", shape=(None, data_size))
+        labels_placeholder = tf.placeholder("int32", shape=None)
+        keep_prob = tf.placeholder("float")
 
         # Build a Graph that computes predictions from the inference model.
-        logits = tf.matmul(data, w) + b
+        logits = inference(data_placeholder, data_size, keep_prob)
+
+        correct_pred = tf.argmax(logits, 1)
 
         # Add to the Graph the loss calculation.
         loss_op = loss(logits, labels_placeholder)
@@ -134,11 +165,11 @@ def run_training(data, labels):
                     keep_prob: FLAGS.keep_prob})
 
             # calculate accuracy in every epoch
-            train_accuracy = sess.run(eval_correct, feed_dict={
+            actual_loss, train_accuracy = sess.run([loss_op, eval_correct], feed_dict={
                 data_placeholder: data,
                 labels_placeholder: labels,
                 keep_prob: FLAGS.keep_prob})
-            print("epoch %d, acc %g" % (epoch, train_accuracy / len(labels)))
+            print("epoch %d, loss %g, acc %g" % (epoch, actual_loss, train_accuracy / len(labels)))
 
             # update TensorBoard
             summary_str = sess.run(summary, feed_dict={
@@ -152,30 +183,74 @@ def run_training(data, labels):
 
         # Save a checkpoint and evaluate the model periodically.
         # Create a saver for writing training checkpoints.
-        path = saver.save(sess, 'models.ckpt')
+        path = saver.save(sess, '/home/hguan/project/machine-learning/garage/models.ckpt')
         print('checkpoint is saved at ' + path)
 
         sess.close()
 
-# Start training
-with tf.Session() as sess:
-    sess.run(init)
+def run_classifier(data):
+    with tf.Graph().as_default():
+        data_size = FLAGS.num_variables
+        num_classes = FLAGS.num_classes
+        data_placeholder = tf.placeholder("float", shape=(None, data_size))
 
-    # Training cycle
-    for epoch in range(training_epochs):
-        # Fit training using batch data
-        _, avg_cost = sess.run([optimizer, cost], feed_dict={x: images32,
-                                                      y: labels})
+        logits = inference(data_placeholder, data_size, 1.0)
+        init_op = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
 
-        # Display logs per epoch step
-        if (epoch+1) % display_step == 0:
-            print("Epoch:", '%04d' % (epoch+1), "cost=", "{:.9f}".format(avg_cost))
+        sess = tf.Session()
+        sess.run(init_op)
 
-    print("Optimization Finished!")
+        saver = tf.train.Saver()
+        saver.restore(sess, '/home/hguan/project/machine-learning/garage/models.ckpt')
 
-    # Test model
-    correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
-    # Calculate accuracy for 3000 examples
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-#    print "Accuracy:", accuracy.eval({x: mnist.test.images[:3000], y: mnist.test.labels[:3000]})
+        prediction_list = []
+        total = len(data)
+        with sess.as_default():
+            for i in range(total):
+                print('#%d : ' % i, end='')
+                v = logits.eval(feed_dict={data_placeholder: [data[i]]})
+                sm = tf.nn.softmax(v)
+                smv = sm.eval()
+                prediction_list.append(smv[0])
 
+                cls = np.argmax(smv)   # get the class number with largest value by argmax
+                # print('prediction=%d (%f)' % (cls, smv[0][cls]))
+
+        return prediction_list
+
+ROOT_PATH = "/home/hguan/project/machine-learning/garage/"
+train_data_dir = os.path.join(ROOT_PATH, "training")
+test_data_dir = os.path.join(ROOT_PATH, "testing")
+
+# Training data
+images, labels = load_data(train_data_dir)
+images_array = np.array(images)
+labels_array = np.array(labels)
+
+# Resize images
+images32 = [transform.resize(image, (28, 28)) for image in images]
+images32 = np.array(images32)
+images32 = rgb2gray(np.array(images32))
+images32 = images32.reshape(-1, 784)
+
+#run_training(images32, labels)
+
+
+# Testing data
+images, labels = load_data(test_data_dir)
+images_array = np.array(images)
+labels_array = np.array(labels)
+
+# Resize images
+images32 = [transform.resize(image, (28, 28)) for image in images]
+images32 = np.array(images32)
+images32 = rgb2gray(np.array(images32))
+images32 = images32.reshape(-1, 784)
+
+#result_list = run_classifier(images32)
+sess = tf.Session()
+predicted = sess.run([correct_pred], feed_dict={}
+
+print(result_list)
+print("==================")
+print(labels)
